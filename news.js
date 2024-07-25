e59c8861f61585809982bc73a5885d14c2548f45
posos-ee <pososee88@gmail.com>
2024-07-25 13:58:48 +0300

    Update news.js

--- news.js
+++ news.js
@@ -1,11 +1,16 @@
+function add_article(title, content, imgs) {
+	var detail = document.createElement("detail");
+
+	document.querySelector("main").appendChild(detail);
+}
+
 function load_newspaper(url) {
 	var xhr = new XMLHttpRequest();
 
 	xhr.open("GET", url, false);
 
 	xhr.onload = function (e) {
-		// Show dialog
-		document.querySelector("main").children[0].children[0].children[0].innerText += (e.target.responseText);
+		var data = e.target.responseXML;
 	}
 	
 	xhr.send();
